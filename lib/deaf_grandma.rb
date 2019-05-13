6e04595cb88d7d3371f8fef15fd94c3a790a50a0
Susan Wendt <sWendt@covermymeds.com>
2019-05-13 09:06:24 -0400

    tasks done

--- lib/deaf_grandma.rb
+++ lib/deaf_grandma.rb
@@ -5,7 +5,7 @@
 class DeafGrandma
 
   def initialize
-    @bye_counter = 0 
+    @bye_counter = 0
   end
 
   def run!
@@ -17,14 +17,28 @@ class DeafGrandma
     end
   end
 
-
   def speak(input)
-
-    #Implement your code here <<<<<<<<<
-
+    if input == "BYE"
+      handle_bye
+    elsif input == input.upcase
+        "NOT SINCE 1964!"
+      else
+        "SPEAK UP SONNY!"
+    end
   end
 
   private
+
+  def handle_bye
+    @bye_counter += 1
+    if @bye_counter == 3
+      p "SEE YOU LATER SONNY!"
+      exit
+    else
+
+      " "
+    end
+  end
 
   def print_welcome
     puts "\nSpeak to your Grandmother: "
@@ -38,4 +52,4 @@ class DeafGrandma
 end
 
 #Uncomment this next line to run your script but BE SURE to comment it, before you try and run your tests.
-#DeafGrandma.new.run!
+DeafGrandma.new.run!
